1098ec74d23bdab24ff7f372d393edfdc229b0ee
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-28 18:31:21 +0200

    Add configuring vim to git setup

--- setup_git.sh
+++ setup_git.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Configure user
 git config --global user.name "Electronic Mango"
 git config --global user.email "78230210+Electronic-Mango@users.noreply.github.com"
 
+# Configure default editor
+git config --global core.editor vim
