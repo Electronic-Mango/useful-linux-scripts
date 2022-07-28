ac6c07837adb4f4692d4b6f93049ca9b80b8588e
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-07-28 18:35:21 +0200

    Add template sourcing all files in "~/.bashrc.d/" directory

---
+++ templates/source_bashrcd_dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Source files in "~/.bashrc.d/" subdirectory.
+# You may want to put all your additions into a separate files
+# in "~/.bashrc.d/", instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+if [ -d "${HOME}/.bashrc.d" ]; then
+    for file in ${HOME}/.bashrc.d/*; do
+        . "${file}"
+    done
+fi
