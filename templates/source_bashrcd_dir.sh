6e859692ec4aa1e85c7a936621e9d683e04532e9
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-10-05 20:27:40 +0200

    Use better checking for files to source
    Now bash shouldn't complain about empty ".bashrc.d" dir.

--- templates/source_bashrcd_dir.sh
+++ templates/source_bashrcd_dir.sh
@@ -4,8 +4,9 @@
 # You may want to put all your additions into a separate files
 # in "~/.bashrc.d/", instead of adding them here directly.
 # See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
 if [ -d "${HOME}/.bashrc.d" ]; then
-    for file in ${HOME}/.bashrc.d/*; do
+    for file in $(find -L ~/.bashrc.d -mindepth 1); do
         . "${file}"
     done
 fi
