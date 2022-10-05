84643feadb5fc8d6cb25b8f4077d102adde93d36
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-10-05 20:20:19 +0200

    Suppress messages when starting SSH agent and adding keys

--- sourced/configure_ssh_agent.sh
+++ sourced/configure_ssh_agent.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-eval "$(ssh-agent -s)"
+eval "$(ssh-agent -s)" > /dev/null
 
 for ssh_key in $(find ~/.ssh/ | grep "id_" | grep -v "\.pub$"); do
-    ssh-add "${ssh_key}"
+    ssh-add -q "${ssh_key}"
 done
 
 trap "kill ${SSH_AGENT_PID}" 0
