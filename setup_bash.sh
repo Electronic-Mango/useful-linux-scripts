a0ba72b6d1b613cccfe2ad31a95646e1068d3765
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-02-22 13:37:21 +0100

    Improve script linking bash aliases
    Check for user confirmation whether to link each path.
    Unify implementation for linking both paths.

--- setup_bash.sh
+++ setup_bash.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+function link() {
+    local target_path="${1}"
+    local source_path="${2}"
+    local additional_info="${3}"
 
-TARGET_BASH_ALIASES="${HOME}/.bash_aliases"
-TARGET_BASHRCD="${HOME}/.bashrc.d"
+    read -p "Replace '${target_path}'? ${additional_info}" replace_confirm
+    if [[ "${replace_confirm,,}" != "y" && "${replace_confirm,,}" != "yes" ]]; then
+        return
+    fi
 
-SOURCE_BASH_ALIASES="${SCRIPT_DIR}/templates/source_bashrcd_dir.sh"
-SOURCE_BASHRCD="${SCRIPT_DIR}/sourced"
+    local script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    local full_source_path="${script_dir}/${source_path}"
+    if [[ -e "${target_path}" ]]; then
+        echo "Removing old '${target_path}' ..."
+        rm -ri "${target_path}"
+    fi
+    echo "Linking $(basename "${target_path}") from '${full_source_path}' to '${target_path}' ..."
+    ln -s "${full_source_path}" "${target_path}"
+}
 
-echo "Removing old configuration files..."
-[[ -f "${TARGET_BASH_ALIASES}" ]] && rm -ri "${TARGET_BASH_ALIASES}"
-[[ -f "${TARGET_BASHRCD}" ]] && rm -ri "${TARGET_BASHRCD}"
+link "${HOME}/.bash_aliases" "templates/source_bashrcd_dir.sh" "CHECK .bashrc IF THIS IS NEEDED! "
+echo
+link "${HOME}/.bashrc.d" "sourced"
 
-echo "Linking .bash_aliases from '${SOURCE_BASH_ALIASES}' to '${TARGET_BASH_ALIASES}'..."
-ln -s "${SOURCE_BASH_ALIASES}" "${TARGET_BASH_ALIASES}"
-
-echo "Linking .bashrc.d from '${SOURCE_BASHRCD}' to '${TARGET_BASHRCD}'..."
-ln -s "${SOURCE_BASHRCD}" "${TARGET_BASHRCD}"
