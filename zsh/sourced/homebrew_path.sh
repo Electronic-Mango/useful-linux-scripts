adbdbcf52844b56f2acecc2c9f32e9bd62b544d2
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-03-27 02:07:43 +0100

    Update zsh/homebrew PATH modifications

--- zsh/sourced/homebrew_path.sh
+++ zsh/sourced/homebrew_path.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
-path=(
-    "${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin:${PATH}"  # coreutils
-    "${HOMEBREW_PREFIX}/opt/python/libexec/bin:${PATH}"  # Python
-    $path
+path+=(
+    "${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin"  # coreutils
+    "${HOMEBREW_PREFIX}/opt/python/libexec/bin"  # Python
+    "${HOMEBREW_PREFIX}/opt/findutils/libexec/gnubin"  # findutils
+    "${HOMEBREW_PREFIX}/opt/grep/libexec/gnubin"  # grep
 )
 export PATH
