aa873a37506df5b07c5e56ec925554681822e3ab
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-07-01 01:03:37 +0200

    Update toolbox setup script

--- setup_toolbox.sh
+++ setup_toolbox.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-if [[ "$(tail -n1 /etc/dnf/dnf.conf)" == "[main]" ]]; then
-    sudo echo 'install_weak_deps=False' >> /etc/dnf/dnf.conf
-fi
-
 sudo dnf --setopt=install_weak_deps=False install \
-    install eza fzf bat ripgrep neovim fd zoxide helix tealdeer \
-    gnome-keyring libsecret
+    eza fzf bat ripgrep neovim fd zoxide tealdeer kitty fish
