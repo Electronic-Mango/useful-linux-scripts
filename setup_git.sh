42b59a9c68acef4433f9d1e0cae6e2e56fbd62e2
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-07-05 23:09:12 +0200

    Use nvim as default git editor

--- setup_git.sh
+++ setup_git.sh
@@ -5,7 +5,7 @@ git config --global user.name "Electronic Mango"
 git config --global user.email "78230210+Electronic-Mango@users.noreply.github.com"
 
 # Configure default editor
-git config --global core.editor vim
+git config --global core.editor nvim
 
 # Configure default branch name
 git config --global init.defaultBranch main
